5ca1cb735d98861a5c6312c0d038d9ff4d9b3491
Mikhail S. Kataev <lpestlname@gmail.com>
2023-08-06 16:47:58 +0300

    Updated demo spawn point

---
+++ Source/UEBuffSystemDemo/DemoClasses/SpawnPoint.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnPoint.generated.h"
+
+UCLASS()
+class UEBUFFSYSTEMDEMO_API ASpawnPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION()
+	virtual void SpawnActor();
+	
+	UFUNCTION()
+	void OnCurrentActorDestroyed(AActor* DestroyedActor);
+
+	void Unsubscribe();	
+protected:
+	UPROPERTY(meta=(AllowPrivateAccess="true"), EditAnywhere, BlueprintReadWrite, Category=Classes)
+	UClass *SpawnActorClass { nullptr };
+
+	UPROPERTY(meta=(AllowPrivateAccess="true"), EditAnywhere, BlueprintReadWrite, Category=Classes)
+	float RespawnTime { 0.f };
+
+private:
+	UPROPERTY()
+	AActor *CurrentActor { nullptr };
+
+	UPROPERTY()
+	FTimerHandle RespawnTimerHandle;
+};
